264bc1270a4aa4dbc5b48cb6282859451db89d80
Richard Caleb Lawver <clawver30@gmail.com>
2022-07-23 10:40:49 -0600

    User schema created

--- models/User.js
+++ models/User.js
@@ -1,7 +1,35 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
+var emailValidator = function(email) {
+    var mail = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$"; 
+    return mail.test(email);
+}
 
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'A username is required!',
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: 'An email is required!',
+        validate: [emailValidator, 'Please input a valid email address']
+    }, 
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
 });
 
 const User = model('User', UserSchema);
