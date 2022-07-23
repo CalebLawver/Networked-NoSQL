f1d26129f706ad6146f5196802c55fb10a971502
Richard Caleb Lawver <clawver30@gmail.com>
2022-07-23 14:16:22 -0600

    routes added

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,15 +1,25 @@
 const router = require('express').Router();
 const {
     // route names
+    allUsers,
+    userById,
+    createUser,
+    updateUser,
+    deleteUser,
+    deleteFriend,
 } = require('../../controllers/user-controller');
 
-router
-    .route('/')
+router.route('/')
+    .get(allUsers)
+    .post(createUser);
 
-router
-    .route('/:id');
+router.route('/:id')
+    .get(userById)
+    .put(updateUser)
+    .delete(deleteUser);
 
-router
-    .route('/:userId/friends/:friendId');
+router.route('/:userId/friends/:friendId')
+        .post(addFriend)
+        .delete(deleteFriend);
 
 module.exports = router;
